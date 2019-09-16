1a42091e7df96f268f00ee89490c64699331f944
TangQiuLiuLiu <55388545+TangQiuLiuLiu@users.noreply.github.com>
2019-09-16 20:02:45 +0800

    修改
    稍作修改后二次提交

--- FirstGitHubClass.java
+++ FirstGitHubClass.java
@@ -2,6 +2,6 @@ package com.test.util;
 
 public class FirstGitHubClass {
 	public static void main(String[] args) {
-		System.out.println("Hello,GitHub!");
+		System.out.println("Hello,GitHub!>>>>>>double");
 	}
 }
